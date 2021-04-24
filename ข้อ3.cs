e320c0886bf595cd41435a819cb181c6be53882b
helloteacher-alt <panyisa20012544@gmail.com>
2021-04-24 15:27:52 +0700

    ข้อ 3

---
+++ ข้อ3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace examGrab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("String mode:");
+            string Mode = Console.ReadLine();
+            Console.Write("Input filmBackWidth:");
+            float filmBackWidth = float.Parse(Console.ReadLine());
+            while (filmBackWidth <= 0)
+            {
+                Console.WriteLine("Invalid filmBackWidth. Please input again.");
+                filmBackWidth = float.Parse(Console.ReadLine());
+            }
+            float fLength, fov;
+            if (Mode == "fLength")
+            {
+                fLength = float.Parse(Console.ReadLine());
+                while (fLength <= 0)
+                {
+                    Console.WriteLine("Invalid filmBackWidth. Please input again.");
+                }
+                fov = 2 * Math.Atan(filmBackWidth / (2 * fLength);
+            }
+        }
+    }
+    }
